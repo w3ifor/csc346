acc6311036234884c188f000411cd457bfdc54d4
w3ifor <34877670+w3ifor@users.noreply.github.com>
2018-02-07 16:32:07 -0600

    Add files via upload
    finish the haversine methed

--- assignment2.java
+++ assignment2.java
@@ -86,6 +86,16 @@ public class assignment2 {
         }
         return vaildzipcode;
     }
+    public static double haversine(double lat1, double lon1, double lat2, double lon2) {
+        final double R = 6371e3;
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+        double Lat = Math.toRadians(lat2 - lat1);
+        double Lon = Math.toRadians(lon2 - lon1);
+        double a = Math.pow(Math.sin(Lat / 2.0),2.0) + Math.cos(lat1) * Math.cos(lat2)* Math.pow(Math.sin(Lon/2.0),2.0);
+        double c = 2.0 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+        return R * c;
+    }
 }
 class Place {
     String zipcode;
